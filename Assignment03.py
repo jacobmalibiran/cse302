b1e9331e64679bd44ba3a5f408ce5c3d6c11a613
jacob <jacobm_2011@yahoo.com>
2026-02-23 17:07:22 -0500

    Co-authored-by: Dylan Kendall <kendaldb@miamioh.edu> Co-authored-by: Tanisha Sikder <thetanishasikder@gmail.com>

--- Assignment03.py
+++ Assignment03.py
@@ -1,23 +1,31 @@
-# returns 2d array of all contents in file
+# Returns array of all contents in the file
 def read_file(file_path):
     data = []
     file = open(file_path, 'r')
     for line in file:
         stripped_line = line.strip()
+        # Data is formatted like "Author: James Kiper"
+        # Gets data after colon
         if ": " in stripped_line:
             type, stripped_line = stripped_line.split(": ", 1)
         data.append(stripped_line)
 
     return data
 
+# Returns dictionary of all keys and data
 def load_dict(data_type, dictionary, data):
+    # Gets common data for all data types
     key = data.pop(0)
     authors = data.pop(0)
     title = data.pop(0)
     author_string = make_author_string(authors)
 
+    # Author and title are common to all file types
+    # Unique_attributes handles the different values
     common_attributes = f"{author_string}, {title}"
     unique_attributes = ""
+
+    # Loads data differently depending on type
     if data_type == "Book":
         unique_attributes = load_book_attributes(data)
     elif data_type == "Journal":
@@ -27,14 +35,14 @@ def load_dict(data_type, dictionary, data):
     
     dictionary[key] = f"{common_attributes}, {unique_attributes}"
     
-
+# Returns unique attributes for book data
 def load_book_attributes(book_data):
     publisher = book_data.pop(0)
     date = book_data.pop(0)
 
     return f"{publisher}, {date}."
 
-# Dictionary of the journal data
+# Returns formatted unique attributes of journal data
 def load_journal_attributes(journal_data):
     journal = journal_data.pop(0)
     publisher = journal_data.pop(0)
@@ -44,7 +52,7 @@ def load_journal_attributes(journal_data):
 
     return f"{journal}, {publisher}:{volume}({number}), {date}."
     
-
+# Finds and returns conference specific data as a string
 def load_conference_attributes(conference_data):
     conference = conference_data.pop(0)
     date = conference_data.pop(0)
@@ -53,40 +61,51 @@ def load_conference_attributes(conference_data):
 
     return f"in Proceedings of {conference}, {location}, {date}, {pages}."
     
+# Extracts and formats authors from data, returning as a string
 def make_author_string(authors):
     author_list = authors.split(", ")
     names_list = []
+    
     for author in author_list:
-        first, last = author.split(" ")
-        names_list.append(last)
-        names_list.append(first)
+        
+        # If the last author has "and" such as ", and Lucasz Opyrchal", we remove it
+        author_name = author.split(" ")
+        # Sometimes, the last author in a list will have "and" preceeding their name
+        if author_name[0] == "and":
+            author_name = author[1:]
+        
+        first_name_idx = 0
+        last_name_idx = 1
+
+        names_list.append(author_name[first_name_idx])
+        names_list.append(author_name[last_name_idx])
         
     author_string = ""
     while names_list:
         last_name = names_list.pop(0)
         first_name = names_list.pop(0)
+
+        # The first author in a list is formatted differently
         if author_string:
             author_string = f"{author_string}, {last_name} {first_name}"
         else:
-            author_string = f"{last_name}, {first_name}"
+            author_string = f"{first_name}, {last_name}"
     return author_string
 
+# Prints formatted data based on a user-input key
 def print_data(library_dictionary, key):
-    print(f"{key}\t{library_dictionary[key]}")
+    # Sample output shows 8 spaces as opposed to a tab.
+    print(f"{key}        {library_dictionary[key]}")
     
 
+# Loads library data from a file into a dictionary, printing an entry based on the user input key
 def main():
-    #todo
-    # fix author parsing
-    # comments/ documentation
-    #Author: Bo Brinkman, Valerie Cross, and Lucasz Opyrchal
-    #Author: Ken Abernethy, John Kelly, Ann Sobel, James Kiper
-    #Abernethy2000        Abernethy, Ken, John Kelly, Ann Sobel, James Kiper, Technology transfer issues for formal methods of software specification, in Proceedings of Thirteenth Conference on Software Engineering Education & Training, Austin, Texas, March 6-8, 2000, 23-31.
-
     library_data = read_file("Data/Step3Data.txt")
     library_dictionary = {}
     
+    # While there is still data in file, keep loading it in the dictionary
     while library_data:
+        # Gets data type if it is a book, journal, or conference
         data_type = library_data.pop(0)
         load_dict(data_type, library_dictionary, library_data)
 
